6c1576a79a634bc52ff98f8b3f77ba250fe30c48
Achsani Taqwim <achsanit87@gmail.com>
2022-06-13 15:44:40 +0800

    add auth google

--- app/src/main/java/com/example/bengkolan/ui/LoginFragment.kt
+++ app/src/main/java/com/example/bengkolan/ui/LoginFragment.kt
@@ -13,14 +13,26 @@ import androidx.navigation.fragment.findNavController
 import com.example.bengkolan.MainActivity
 import com.example.bengkolan.R
 import com.example.bengkolan.databinding.FragmentLoginBinding
-import com.example.bengkolan.databinding.FragmentRegisterBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding?= null
     private val binding get() = _binding!!
     lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
     private var email = ""
     private var password = ""
 
@@ -36,6 +48,7 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         firebaseAuth = FirebaseAuth.getInstance()
+        db = Firebase.firestore
 
         binding.btnMasuk.setOnClickListener {
             //sebelum login di validasi dlu datanya
@@ -50,10 +63,74 @@ class LoginFragment : Fragment() {
         binding.tvForget.setOnClickListener {
             resetPassword()
         }
+
+        binding.btnMasukGoogle.setOnClickListener {
+            authGoogle()
+        }
     }
 
     fun onBackPressed() {
         Toast.makeText(activity, "Back button disabled", Toast.LENGTH_SHORT)
+    }
+
+    private fun authGoogle() {
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .requestProfile()
+            .build()
+
+        val signInClient = GoogleSignIn.getClient(requireActivity(), options)
+        signInClient.signInIntent.also {
+            startActivityForResult(it, REQUEST_CODE_SIGN_IN)
+        }
+    }
+
+    private fun googleAuthForFirebase(account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                firebaseAuth.signInWithCredential(credentials).await()
+                withContext(Dispatchers.Main) {
+                    val account = GoogleSignIn.getLastSignedInAccount(context)
+                    if (account != null) {
+                        val personName = account.displayName
+                        val personEmail =account.email
+
+                        val currentUser = firebaseAuth.currentUser
+
+                        if (currentUser!=null) {
+                            val data = hashMapOf(
+                                "username" to personName,
+                                "email" to personEmail
+                            )
+
+                            db.collection("users")
+                                .document(firebaseAuth.currentUser!!.uid)
+                                .set(data)
+                        }
+
+                    }
+
+                    startActivity(Intent(activity, MainActivity::class.java))
+                    Toast.makeText(requireContext(), "Successfully logged in", Toast.LENGTH_LONG).show()
+                }
+            } catch(e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE_SIGN_IN) {
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
+            account?.let {
+                googleAuthForFirebase(it)
+            }
+        }
     }
 
     private fun resetPassword() {
@@ -115,5 +192,9 @@ class LoginFragment : Fragment() {
                 binding.progressLogin.visibility = View.GONE
                 Toast.makeText(activity, "Login Failed", Toast.LENGTH_SHORT).show()
             }
+    }
+
+    companion object {
+        const val REQUEST_CODE_SIGN_IN = 999
     }
 }
